b5ed33c5392f305653c5b50e00737aaa379575b5
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-07-03 11:35:59 +0100

    Event_Booking

---
+++ src/main/java/com/BeeJay_Event_Booking_App/My_App/exceptions/EventBaseException.java
@@ -0,0 +1,7 @@
+package com.BeeJay_Event_Booking_App.My_App.exceptions;
+
+public class EventBaseException extends Throwable{
+    public EventBaseException(String message){
+        super(message);
+    }
+}
