8bdc749e5e43e86b20fbebbd538c64ff6b9fe0ec
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-07-03 11:40:56 +0100

    Models_Done_And_Created

--- src/main/java/com/BeeJay_Event_Booking_App/My_App/models/Discount.java
+++ src/main/java/com/BeeJay_Event_Booking_App/My_App/models/Discount.java
@@ -1,4 +1,10 @@
 package com.BeeJay_Event_Booking_App.My_App.models;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Discount {
 }
--- src/main/java/com/BeeJay_Event_Booking_App/My_App/models/Organizer.java
+++ src/main/java/com/BeeJay_Event_Booking_App/My_App/models/Organizer.java
@@ -1,4 +1,9 @@
 package com.BeeJay_Event_Booking_App.My_App.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Organizer {
 }
